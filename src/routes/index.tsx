a7e96753ba9c6abc738ac7ce61e8119729196bcd
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-13 17:45:13 +0100

    refactor: :recycle: add alias to imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,18 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import ErrorPage from '../ErrorPage';
-import Albums from '../modules/Albums/components';
-import Photos from '../modules/Photos/components';
-import Users from '../modules/Users/components';
-
-import HomePage from '../modules/HomePage/components';
-import AppLayout from '../components/AppLayout';
+import AlbumsPage from '@/modules/Albums/components';
+import PhotosPage from '@/modules/Photos/components';
+import UsersPage from '@/modules/Users/components';
+import HomePage from '@/modules/HomePage/components';
+import AppLayout from '@/components/AppLayout';
+import ErrorPage from '@/ErrorPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<AppLayout />} errorElement={<ErrorPage />}>
       <Route path="/" element={<HomePage />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/albums" element={<Albums />} />
-      <Route path="/photos" element={<Photos />} />
+      <Route path="/users" element={<UsersPage />} />
+      <Route path="/albums" element={<AlbumsPage />} />
+      <Route path="/photos" element={<PhotosPage />} />
     </Route>
   )
 );
