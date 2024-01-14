41aab867f2fc85d9d04c64e674dd43005f76cabf
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 18:31:03 +0100

    feat: :lipstick: restyle header

--- src/ui/components/Header/index.tsx
+++ src/ui/components/Header/index.tsx
@@ -2,37 +2,13 @@ import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar w-full bg-base-100">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div aria-label="Header Menu" tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
-          </div>
-          <ul className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/albums">Albums</Link>
-            </li>
-            <li>
-              <Link to="/photos">Photos</Link>
-            </li>
-          </ul>
-        </div>
         <Link to="/" className="btn btn-ghost text-xl">
           Sushi
         </Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      <div className="navbar-end lg:flex">
         <ul className="menu menu-horizontal gap-4 px-1">
           <li>
             <Link to="/users">Users</Link>
