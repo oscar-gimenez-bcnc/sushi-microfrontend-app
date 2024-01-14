9848abc5a328a0148536b5656e3baf90f57d39f8
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 18:07:22 +0100

    refactor: :recycle: remove bootstrap file in shell app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './ui/styles/globals.css';
 import { ErrorBoundary } from 'react-error-boundary';
-import GenericError from './ui/components/GenericError';
 import AppRouter from './routes';
+import GenericError from './ui/components/GenericError';
 import { CacheProvider } from './ui/contexts/CacheContext';
 
-const App: React.FC = () => {
-  return (
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
     <div className="container mx-auto">
       <ErrorBoundary fallback={<GenericError />}>
         <CacheProvider>
@@ -12,7 +17,5 @@ const App: React.FC = () => {
         </CacheProvider>
       </ErrorBoundary>
     </div>
-  );
-};
-
-export default App;
+  </React.StrictMode>
+);
