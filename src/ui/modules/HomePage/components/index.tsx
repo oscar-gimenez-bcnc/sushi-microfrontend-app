7f5c3044a38d2f8ff56bbcbc41391c440c3c72a2
OscarGimenez <o.gimenez.aldabas@gmail.com>
2024-01-14 20:39:46 +0100

    feat: :sparkles: add homepage

--- src/ui/modules/HomePage/components/index.tsx
+++ src/ui/modules/HomePage/components/index.tsx
@@ -1,5 +1,22 @@
+import type React from 'react';
+
 const HomePage: React.FC = () => {
-  return <>HomePage</>;
+  return (
+    <>
+      <div className="flex flex-row items-center rounded-xl bg-indigo-100 p-8 text-center">
+        <div className="w-2/3  pl-8">
+          <h1 className="mb-2 text-4xl font-bold text-gray-900">Sushi Photos</h1>
+          <h2 className="mb-4 text-2xl text-gray-700">A microfrontend usage example</h2>
+          <p className="text-gray-600">
+            Discover here the best way to display your photos. Join us in Sushi Photos and enjoy sushi!
+          </p>
+        </div>
+        <div className="w-1/3">
+          <img src="./sushi.jpg" alt="main-image" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
