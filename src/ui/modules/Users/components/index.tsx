b85dabc0ee782d871f7ae3f37d530ac897c4311a
Óscar Giménez Aldabas <oscar.gimenez@bcncgroup.com>
2024-03-13 16:11:51 +0100

    feat: :sparkles: remove id param

--- src/ui/modules/Users/components/index.tsx
+++ src/ui/modules/Users/components/index.tsx
@@ -1,17 +1,15 @@
 import MicroFrontendLoader from '@/ui/components/MicroFrontendLoader';
 import { CacheContext } from '@/ui/contexts/CacheContext';
 import React, { Suspense, useContext } from 'react';
-import { useParams } from 'react-router-dom';
 
 const SushiMicroFrontendUsers = React.lazy(async () => await import('SushiMicroFrontendUsers/SushiMicroFrontendUsers'));
 
 const UsersPage: React.FC = () => {
   const { userCacheActions } = useContext(CacheContext);
-  const { id } = useParams<{ id?: string }>();
 
   return (
     <Suspense fallback={<MicroFrontendLoader />}>
-      <SushiMicroFrontendUsers cacheActions={userCacheActions} userId={id} />
+      <SushiMicroFrontendUsers cacheActions={userCacheActions} />
     </Suspense>
   );
 };
